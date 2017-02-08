18cdecf8244d07790240f57a65a576d8b3e3b9ea
Peter Širka <petersirka@gmail.com>
2017-02-08 21:38:56 +0100

    New improvements.

--- public/js/ui.js
+++ public/js/ui.js
@@ -991,6 +991,7 @@ COMPONENT('codemirror', function() {
 	var editor;
 	var timeout;
 	var isTyping = false;
+	var currentH;
 
 	self.validate = function(value) {
 		return required ? value && value.length > 0 : true;
@@ -1028,7 +1029,7 @@ COMPONENT('codemirror', function() {
 		var container = self.find('.ui-codemirror');
 		var maxlength = (self.attr('data-maxlength') || '').parseInt();
 
-		editor = CodeMirror(container.get(0), { lineNumbers: self.attr('data-linenumbers') === 'true', mode: self.attr('data-type') || 'htmlmixed', indentUnit: 4, placeholder: self.attr('data-placeholder'), extraKeys: { 'Enter': function() { return self.enter(0); }, 'Cmd-Enter': function() { return self.enter(1); }, 'Up': function() { if (editor.getValue()) return CodeMirror.Pass; self.edit(true); }, 'Esc': function() { self.edit(false); return CodeMirror.pass; }, 'Ctrl-Enter': function() { return self.enter(1); }}});
+		editor = CodeMirror(container.get(0), { lineNumbers: self.attr('data-linenumbers') === 'true', lineWrapping: true, mode: self.attr('data-type') || 'htmlmixed', indentUnit: 4, placeholder: self.attr('data-placeholder'), extraKeys: { 'Enter': function() { return self.enter(0); }, 'Cmd-Enter': function() { return self.enter(1); }, 'Up': function() { if (editor.getValue()) return CodeMirror.Pass; self.edit(true); }, 'Esc': function() { self.edit(false); return CodeMirror.pass; }, 'Ctrl-Enter': function() { return self.enter(1); }}});
 
 		editor.on('dragover', function() {
 			self.element.addClass('ui-codemirror-dragdrop');
@@ -1045,9 +1046,12 @@ COMPONENT('codemirror', function() {
 
 		maxlength && editor.setOption('maxlength', maxlength);
 
-		editor.on('viewportChange', function() {
-			EMIT('resize');
-			scrollBottom();
+		editor.on('update', function() {
+			var h = editor.getScrollerElement().clientHeight;
+			if (currentH !== h) {
+				currentH = h;
+				EMIT('resize');
+			}
 		});
 
 		editor.on('keypress', function() {
@@ -1462,10 +1466,15 @@ COMPONENT('typing', function() {
 	var self = this;
 	var cache = {};
 	var empty = true;
+	var count = 0;
+	var max;
 
 	self.readonly();
 
 	self.make = function() {
+
+		max = (self.attr('data-max') || '4').parseInt();
+
 		self.classes('ui-typing');
 		var scr = self.find('script');
 		self.template = Tangular.compile(scr.html());
@@ -1478,20 +1487,25 @@ COMPONENT('typing', function() {
 				if (cache[id] > dt)
 					return;
 				delete cache[id];
+				count--;
 				self.find('div[data-id="{0}"]'.format(id)).remove();
 			});
 		}, 5000);
 	};
 
 	self.clear = function() {
+		count = 0;
 		cache = {};
 		self.empty();
 	};
 
 	self.insert = function(user) {
+		if (count > max)
+			return;
 		var is = cache[user.id] ? true : false;
 		cache[user.id] = new Date().add('5 seconds');
 		empty = false;
+		count++;
 		!is && self.append(self.template(user));
 	};
 });
