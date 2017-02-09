4710e8975a637a1c0f6fb1e05c682e2295ebb6da
Peter Širka <petersirka@gmail.com>
2017-02-09 13:25:50 +0100

    Add a new component `template`.

--- public/js/ui.js
+++ public/js/ui.js
@@ -505,6 +505,38 @@ COMPONENT('error', function() {
 	};
 });
 
+COMPONENT('template', function() {
+	var self = this;
+	self.readonly();
+	self.make = function(template) {
+
+		if (template) {
+			self.template = Tangular.compile(template);
+			return;
+		}
+
+		var script = self.find('script');
+
+		if (!script.length) {
+			script = self.element;
+			self.element = self.element.parent();
+		}
+
+		self.template = Tangular.compile(script.html());
+		script.remove();
+	};
+
+	self.setter = function(value) {
+		if (NOTMODIFIED(self.id, value))
+			return;
+		if (!value)
+			return self.classes('hidden');
+		KEYPRESS(function() {
+			self.html(self.template(value)).removeClass('hidden');
+		}, 100, self.id);
+	};
+});
+
 COMPONENT('page', function() {
 	var self = this;
 	var isProcessed = false;
