18a8e22807367399bb0b3d888b6d368da5fcc39f
Peter Širka <petersirka@gmail.com>
2017-02-10 14:26:12 +0100

    Improve sending notifications.

--- definitions/scheduler.js
+++ definitions/scheduler.js
@@ -1,4 +1,5 @@
 // User's notifications
 F.schedule('08:00', '1 day', function() {
-	OPERATION('users.notify', NOOP);
+	var time = ('1' + F.datetime.format('HHmm')).parseInt();
+	(time > 10755 && time < 10825) && OPERATION('users.notify', NOOP);
 });
