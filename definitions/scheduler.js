d6fb5d836e26355d3ef48ce62897d61dec359e3f
Peter Širka <petersirka@gmail.com>
2017-02-10 20:04:08 +0100

    Rename method.

--- definitions/scheduler.js
+++ definitions/scheduler.js
@@ -1,5 +1,5 @@
 // User's notifications
-F.schedule('08:00', '1 day', function() {
+SCHEDULE('08:00', '1 day', function() {
 	var time = ('1' + F.datetime.format('HHmm')).parseInt();
 	(time > 10755 && time < 10825) && OPERATION('users.notify', NOOP);
 });
