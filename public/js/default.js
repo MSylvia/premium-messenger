b9ccdcdcdac3db4173a949733b93b8b68a6e8f74
Peter Širka <petersirka@gmail.com>
2017-02-11 19:17:54 +0100

    Fix `smilefy`.

--- public/js/default.js
+++ public/js/default.js
@@ -80,6 +80,8 @@ function smilefy(str) {
 function smilefy2(str, wrap) {
 	var db = { ':-)': 1, ':)': 1, ';)': 8, ':D': 0, '8)': 5, ':((': 7, ':(': 3, ':|': 2, ':P': 6, ':O': 4, ':*': 9, '+1': 10, '1': 11, '\/': 12 };
 	return str.replace(/(\-1|[:;8O\-)DP(|\*]|\+1){1,3}/g, function(match) {
+		if (match === '-1')
+			return match;
 		var smile = db[match.replace('-', '')];
 		return smile === undefined ? match : '<i class="smiles smiles-' + smile + '"></i>';
 	});
